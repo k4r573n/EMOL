1db904c0962aa19d41b99b994a0f7b45879f8e81
Karsten Hinz <k.hinz@tu-bs.de>
2011-08-16 17:44:59 +0200

    - gibt osm link zu WP aus wenn markiert - zeigt jetzt auch google karten - versuche die HitchMap einzubinden

--- download_js_css.sh
+++ download_js_css.sh
@@ -1,3 +1,4 @@
 wget http://openlayers.org/dev/theme/default/style.css
 wget http://openlayers.org/dev/OpenLayers.js
 wget http://www.openstreetmap.org/openlayers/OpenStreetMap.js
+wget http://code.jquery.com/jquery-1.6.2.min.js
