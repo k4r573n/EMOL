71203c51e452679042db374a95415e2223c80e92
Karsten Hinz <k.hinz@tu-bs.de>
2011-08-29 14:54:18 +0200

    missing libs in download included

--- download_js_css.sh
+++ download_js_css.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+wget http://jqueryui.com/download/jquery-ui-1.8.16.custom.zip
+unzip jquery-ui-1.8.16.custom.zip
+rm jquery-ui-1.8.16.custom.zip
+
 mkdir js
 cd js
 wget http://openlayers.org/dev/OpenLayers.js
 wget http://www.openstreetmap.org/openlayers/OpenStreetMap.js
 wget http://code.jquery.com/jquery-1.6.2.min.js
 wget https://jquery-json.googlecode.com/files/jquery.json-2.2.min.js
+
+wget http://plugins.jquery.com/files/jquery.scrollTo-1.4.2.zip
+unzip jquery.scrollTo-1.4.2.zip
+rm jquery.scrollTo-1.4.2.zip
+
 
 mkdir theme
 mkdir theme/default
